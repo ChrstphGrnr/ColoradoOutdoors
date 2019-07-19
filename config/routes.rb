0a6a0b0d38098b40e45863eb8cad13e9a08b3702
chrstphgrnr <christophgruener@me.com>
2019-07-19 10:30:49 -0600

    change route for attractions with namespacing for API calls

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
-      resources :users
-      resources :parks
+      resources :attractions
     end 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
