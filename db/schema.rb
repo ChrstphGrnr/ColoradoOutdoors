0648052144e36b418381eb66cf5e6b2e1323a31d
chrstphgrnr <christophgruener@me.com>
2019-07-19 10:31:14 -0600

    modify schema for db

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_18_161227) do
+ActiveRecord::Schema.define(version: 2019_07_18_230231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "attractions", force: :cascade do |t|
     t.string "name"
-    t.string "username"
-    t.string "password_digest"
+    t.string "location", array: true
+    t.string "description"
+    t.string "images", array: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
