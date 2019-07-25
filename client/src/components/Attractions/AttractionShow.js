75e97139de6d16b0b0d8214ecafd55d816133c5f
chrstphgrnr <christophgruener@me.com>
2019-07-25 17:15:57 -0600

    add AttractionShow component for individual Attraction Show page

---
+++ client/src/components/Attractions/AttractionShow.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AttractionShow = (props) => {
+    
+        debugger
+        return (
+            <div>
+                {console.log(props.location.state)}
+            </div>
+        );
+}
+
+
+export default AttractionShow;
