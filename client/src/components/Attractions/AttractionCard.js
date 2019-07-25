36137ae34399559a4f04f8652f242040e493391e
chrstphgrnr <christophgruener@me.com>
2019-07-25 17:11:03 -0600

    rename file and component to AttractionCard, add link to individual resource

---
+++ client/src/components/Attractions/AttractionCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import { Link } from 'react-router-dom'
+
+const AttractionCard = (props) => {
+        const attraction = props.attraction
+       
+        return (
+            
+            <div>
+
+            <Card className="bg-dark text-white" style={{ width: '56rem'}}>
+              <Card.Img variant="top" src={attraction.images[1]} />
+              <Card.Body>
+                <Card.Title >{attraction.name}</Card.Title>
+                <Card.Text >
+                {attraction.description}
+                </Card.Text>
+                <Link to={{pathname:`/attractions/${attraction.name}`,                  state: {attraction: attraction}} }>                    
+                <Button variant="primary">See this Attraction</Button>
+                </Link>
+              </Card.Body>
+            </Card>
+            <br/>
+            <br/>
+
+            </div>
+           
+        );
+    }
+
+
+export default AttractionCard;
+
