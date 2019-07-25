d208e673a87787e08f9c86e52ee037912559654b
chrstphgrnr <christophgruener@me.com>
2019-07-25 17:16:43 -0600

    add containers folder

---
+++ client/src/containers/AttractionIndex.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import AttractionCard from '../components/Attractions/AttractionCard'
+import { fetchAttractions } from '../actions/attractions'
+import { connect } from 'react-redux';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+class AttractionIndex extends Component {
+
+    componentDidMount() {
+        this.props.fetchAttractions();
+    }
+
+
+
+    render() {
+        console.log(this.props)
+
+        // const halfAttractions = this.props.attractions.length/2
+        // debugger
+            return (
+                <div >
+                    <Row>
+                        <Col>
+                        {this.props.attractions.map(attraction => <AttractionCard attraction={attraction} />)}   
+                        </Col>
+                    </Row>
+                </div>
+            );
+        }
+    }
+
+    const mapStateToProps = state =>{
+        return {
+          attractions: state.attraction,
+        //   searchTerm:state.search
+        }
+      }
+      
+       const mapDispatchToProps = dispatch =>{
+        return{
+          fetchAttractions: () => dispatch(fetchAttractions()),
+        //   setSearch: (searchTerm) => dispatch(setSearch(searchTerm))
+        }
+      
+       }
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(AttractionIndex);
