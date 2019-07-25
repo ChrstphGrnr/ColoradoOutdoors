1be72d4aa65ea8d4607091c8c3f9a045c056927d
chrstphgrnr <christophgruener@me.com>
2019-07-24 18:15:15 -0600

    change filestructure in actions folder

---
+++ client/src/actions/attractions.js
@@ -0,0 +1,23 @@
+// import attractions from "../reducers/attractions";
+
+
+// async action
+export const fetchAttractions = () => {
+    return (dispatch) => {
+        dispatch({ type: 'COMMENCE_ATTRACTION_FETCH' })
+        return fetch("http://localhost:3001/api/v1/attractions")
+        .then(resp => resp.json())
+        .then(attractions =>dispatch({type:"SET_MON", attractions} ))
+        .then(x => console.log('fetch complete'))
+
+       };
+}
+
+
+
+ // action creator
+export const setSearch = (searchTerm) =>{
+    return{
+        type:"SET_SEARCH", search:searchTerm
+    }
+} 
