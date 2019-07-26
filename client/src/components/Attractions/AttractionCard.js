6f05827058c55ddb2d9de92a5e1e5eabd7844e10
chrstphgrnr <christophgruener@me.com>
2019-07-25 21:12:18 -0600

    add Link/Button to attraction with routing

--- client/src/components/Attractions/AttractionCard.js
+++ client/src/components/Attractions/AttractionCard.js
@@ -5,19 +5,21 @@ import { Link } from 'react-router-dom'
 
 const AttractionCard = (props) => {
         const attraction = props.attraction
+        const id = props.id
        
         return (
             
             <div>
 
-            <Card className="bg-dark text-white" style={{ width: '56rem'}}>
+            <Card id={id} className="bg-dark text-white" style={{ width: '56rem'}}>
               <Card.Img variant="top" src={attraction.images[1]} />
               <Card.Body>
                 <Card.Title >{attraction.name}</Card.Title>
                 <Card.Text >
                 {attraction.description}
                 </Card.Text>
-                <Link to={{pathname:`/attractions/${attraction.name}`,                  state: {attraction: attraction}} }>                    
+                <Link to={{pathname:`/attractions/${attraction.name}`,
+                                  state: {attraction: attraction}} }>                    
                 <Button variant="primary">See this Attraction</Button>
                 </Link>
               </Card.Body>
