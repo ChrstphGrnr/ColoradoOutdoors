08071e4dcea775737e5f74d1f849f556fd3824b8
chrstphgrnr <christophgruener@me.com>
2019-07-25 21:20:12 -0600

    change column name for username in comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_163618) do
+ActiveRecord::Schema.define(version: 2019_07_26_020701) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2019_07_23_163618) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
-    t.string "username"
+    t.string "name"
     t.integer "attraction_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
