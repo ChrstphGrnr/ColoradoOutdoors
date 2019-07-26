1d761de03c07ea3aad4ee28c2cf7b24be8f14abe
chrstphgrnr <christophgruener@me.com>
2019-07-25 21:17:38 -0600

    add connect

---
+++ client/src/containers/CommentIndex.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class CommentIndex extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default connect(null, mapDispatchToProps)(CommentIndex);
