0e28d711bd5f43ce366d981a306aee313ab358ff
chrstphgrnr <christophgruener@me.com>
2019-07-25 21:11:06 -0600

    add post action for comments

--- client/src/actions/comments.js
+++ client/src/actions/comments.js
@@ -1,13 +1,23 @@
-export const fetchAttractions = () => {
-    return (dispatch) => {
-        return fetch("http://localhost:3001/api/v1/attractions")
-        .then(resp => resp.json())
-        .then(attractions =>dispatch({type:"SET_MON", attractions} ))
-        .then(x => console.log('fetch complete'))
-
-       };
+export const addComment = (comment) => {
+    const { name, content } = comment 
+        return (dispatch) => {
+            return fetch('http://localhost:3001/comments', {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json', 
+                    Accept: 'application/json'
+                }, 
+                body: JSON.stringify({
+                    name, 
+                    content
+                })
+                .then(resp => resp.json())
+                .then(comment => dispatch({type: "ADD_COMMENT", comment}))
+                .catch(error => console.log(error))
+            })
+        
+    } 
 }
-
 
 
  // action creator
