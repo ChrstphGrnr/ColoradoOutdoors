7fe3f93dd3c4d24c3fb2b2335d87afb42e9520a0
chrstphgrnr <christophgruener@me.com>
2019-07-25 21:10:40 -0600

    change action setter

--- client/src/actions/attractions.js
+++ client/src/actions/attractions.js
@@ -6,7 +6,7 @@ export const fetchAttractions = () => {
         dispatch({ type: 'COMMENCE_ATTRACTION_FETCH' })
         return fetch("http://localhost:3001/api/v1/attractions")
         .then(resp => resp.json())
-        .then(attractions =>dispatch({type:"SET_MON", attractions} ))
+        .then(attractions =>dispatch({type:"SET_ATTRACTIONS", attractions} ))
         .then(x => console.log('fetch complete'))
 
        };
