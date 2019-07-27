7be02f42583bcb7e0905fbe4a24ecd0fbdcfbfeb
chrstphgrnr <christophgruener@me.com>
2019-07-26 21:41:05 -0600

    add search term and search term mapping for attractions

--- client/src/containers/AttractionIndex.js
+++ client/src/containers/AttractionIndex.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import AttractionCard from '../components/Attractions/AttractionCard'
-import { fetchAttractions } from '../actions/attractions'
+import { fetchAttractions} from '../actions/attractions'
 import { connect } from 'react-redux';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -11,39 +11,33 @@ class AttractionIndex extends Component {
         this.props.fetchAttractions();
     }
 
-
-
     render() {
-        console.log(this.props)
-
-        // const halfAttractions = this.props.attractions.length/2
-        // debugger
+        const attractions = this.props.attractions.filter(attraction => attraction.name.includes(this.props.searchTerm))
             return (
                 <div >
                     <Row>
                         <Col>
-                        {this.props.attractions.map(attraction => <AttractionCard id={attraction.id} attraction={attraction} />)}   
+                        {attractions.map(attraction => <AttractionCard key={attraction.id} id={attraction.id} attraction={attraction} />)}   
                         </Col>
                     </Row>
                 </div>
             );
         }
     }
-
     const mapStateToProps = state =>{
         return {
-          attractions: state.attraction,
-        //   searchTerm:state.search
+        
+          attractions: state.attractions,
+          searchTerm: state.search
         }
       }
-      
-       const mapDispatchToProps = dispatch =>{
-        return{
-          fetchAttractions: () => dispatch(fetchAttractions()),
-        //   setSearch: (searchTerm) => dispatch(setSearch(searchTerm))
-        }
-      
-       }
+    const mapDispatchToProps = dispatch =>{
+     return{
+       fetchAttractions: () => dispatch(fetchAttractions()),
+     //   setSearch: (searchTerm) => dispatch(setSearch(searchTerm))
+     }
+    
+    }
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(AttractionIndex);
