87b52947e275b667de88a82fe663093ba4110d12
chrstphgrnr <christophgruener@me.com>
2019-07-26 21:32:26 -0600

    change post body to allow for attraction_id

--- client/src/actions/comments.js
+++ client/src/actions/comments.js
@@ -1,28 +1,23 @@
-export const addComment = (comment) => {
-    const { name, content } = comment 
+export const addComment = (comment, attractionId) => {
+    // debugger    
+        const attraction_id = attractionId
+        const commentContent = {...comment, attraction_id}
+
         return (dispatch) => {
-            return fetch('http://localhost:3001/comments', {
+            return fetch('http://localhost:3001/api/v1/comments', {
                 method: 'POST', 
                 headers: {
                     'Content-Type': 'application/json', 
                     Accept: 'application/json'
                 }, 
                 body: JSON.stringify({
-                    name, 
-                    content
+                    commentContent
                 })
+            })
                 .then(resp => resp.json())
                 .then(comment => dispatch({type: "ADD_COMMENT", comment}))
                 .catch(error => console.log(error))
-            })
-        
     } 
 }
 
 
- // action creator
-export const setSearch = (searchTerm) =>{
-    return{
-        type:"SET_SEARCH", search:searchTerm
-    }
-} 
