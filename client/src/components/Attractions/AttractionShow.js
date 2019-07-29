b78ee994ebc67b9b506de649275d669d5fa6aaa9
chrstphgrnr <christophgruener@me.com>
2019-07-29 17:22:39 -0600

    add Navbar render

--- client/src/components/Attractions/AttractionShow.js
+++ client/src/components/Attractions/AttractionShow.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
 import CommentContainer from '../../containers/CommentContainer'
+import Navigation from '../Navbar'
 
 const AttractionShow = (props) => {
-        // debugger
         const attraction = props.location.state.attraction
         return (
             <div>
+            <Navigation sticky="top"/>
             {console.log(props.location.state)}
                 <Card id={attraction.id} className="bg-dark text-white" style={{ width: '56rem'}}>
 
@@ -15,19 +16,14 @@ const AttractionShow = (props) => {
                     <br/>
                    
                     <Card.Body>
-                        <Card.Title >{attraction.name}</Card.Title>
+                        <Card.Title ><h1>{attraction.name}</h1></Card.Title>
                         <Card.Text >
                         {attraction.description}
 
                         </Card.Text>
                         <br/>
                         <br/>
-                        <CommentContainer attractionId={attraction.id} />
-                            
-                            {/* <ul>
-                              {attraction.comments.map(comment=> <CommentShow key={comment.id} comment={comment} /> )}
-                            </ul>
-                        <CommentForm attractionId={attraction.id} /> */}
+                        <CommentContainer attractionId={attraction.id} />  
                     </Card.Body>
                 </Card>
             </div>
