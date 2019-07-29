3123fc007a6320d9d8d7bbc66c28ec3bb9cc5172
chrstphgrnr <christophgruener@me.com>
2019-07-29 17:23:48 -0600

    change html output

--- client/src/components/Comments/CommentShow.js
+++ client/src/components/Comments/CommentShow.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import Button from 'react-bootstrap/Button'
 
 
 const CommentShow = (props) => {  
         return (
             <div>
+                
+                <br/>
                 <h5>
                     {props.comment.content}
                 </h5>
@@ -12,6 +14,8 @@ const CommentShow = (props) => {
                     by: {props.comment.name} 
                 </h6>
                 <br/>
+                <br/>
+                -------------------------------
             </div>
         );
 }
