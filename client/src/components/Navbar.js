5e62cabcc7ba70a29f58d2756b70ac88115a6943
chrstphgrnr <christophgruener@me.com>
2019-07-30 09:52:32 -0600

    refactor conditional rendering of search box

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,61 +6,38 @@ import { Search } from 'semantic-ui-react'
 
 class Navigation extends Component {
 
-
-
   search = (e, {value}) =>{
     this.props.setSearch(value)
   }
 
-
-    render() {
-        if (this.props.search === 'true' ) {
-          return (
-            <div>
-            <Navbar variant="dark" bg="dark" expand="lg" sticky="top">
-               <Navbar.Brand  href="/">Colorado Outdoors</Navbar.Brand>
-               <Navbar.Toggle aria-controls="basic-navbar-nav" />
-               <Navbar.Collapse id="basic-navbar-nav">
-                 <Nav className="mr-auto">
-                  <Nav.Link href="/">Home</Nav.Link>
-                  <NavDropdown  title="More Info" id="basic-nav-dropdown">
-                    <NavDropdown.Item  href="http://www.recreation.gov">Recreation.gov</NavDropdown.Item>
-                    <NavDropdown.Divider/>
-                    <NavDropdown.Item  href="http://www.nps.gov">National Park Service</NavDropdown.Item>
-                    <NavDropdown.Divider/>
-                    <NavDropdown.Item  href="/about">About this App</NavDropdown.Item>
-                  </NavDropdown>
-                 </Nav>
-                </Navbar.Collapse>
-                <Search onSearchChange={_.debounce(this.search, 500)} showNoResults={false} type="text" placeholder="Search" className="sm" />
-            </Navbar>
-            </div>
-            ); 
-        } else {
-          return (
-            <div>
-            <Navbar variant="dark" bg="dark" expand="lg" sticky="top">
-               <Navbar.Brand  href="/">Colorado Outdoors</Navbar.Brand>
-               <Navbar.Toggle aria-controls="basic-navbar-nav" />
-               <Navbar.Collapse id="basic-navbar-nav">
-                 <Nav className="mr-auto">
-                  <Nav.Link href="/">Home</Nav.Link>
-                  <NavDropdown  title="More Info" id="basic-nav-dropdown">
-                    <NavDropdown.Item  href="http://www.recreation.gov">Recreation.gov</NavDropdown.Item>
-                    <NavDropdown.Divider/>
-                    <NavDropdown.Item  href="http://www.nps.gov">National Park Service</NavDropdown.Item>
-                    <NavDropdown.Divider/>
-                    <NavDropdown.Item  href="/about">About this App</NavDropdown.Item>
-                  </NavDropdown>
-                 </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-            </div>
-            ); 
-        }
-    }
-
-        
+  render() {
+    const searchFunction = () => {
+      if (this.props.search === 'true') {
+        return <Search onSearchChange={_.debounce(this.search, 500)} showNoResults={false} type="text" placeholder="Search" className="sm" />
+      } 
+    } 
+    return (
+      <div>
+      <Navbar variant="dark" bg="dark" expand="lg" sticky="top">
+         <Navbar.Brand  href="/">Colorado Outdoors</Navbar.Brand>
+         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+         <Navbar.Collapse id="basic-navbar-nav">
+           <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <NavDropdown  title="More Info" id="basic-nav-dropdown">
+              <NavDropdown.Item  href="http://www.recreation.gov">Recreation.gov</NavDropdown.Item>
+              <NavDropdown.Divider/>
+              <NavDropdown.Item  href="http://www.nps.gov">National Park Service</NavDropdown.Item>
+              <NavDropdown.Divider/>
+              <NavDropdown.Item  href="/about">About this App</NavDropdown.Item>
+            </NavDropdown>
+           </Nav>
+          </Navbar.Collapse>
+          {searchFunction()}
+      </Navbar>
+      </div>
+    );    
+  }      
 }
 
 
